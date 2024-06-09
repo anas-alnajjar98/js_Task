ae683c928db2cac66eff019ad47c1dca7c03a87c
anas-alnajjar98 <160442902+anas-alnajjar98@users.noreply.github.com>
2024-06-09 20:16:12 +0300

    Update script.js

--- js-task3/script.js
+++ js-task3/script.js
@@ -46,5 +46,44 @@ p.style.fontSize = size.value;
         p.style.textDecoration="none";
     }
  });
+let s4=document.getElementById("div_cont");
+ s4.style.width="200px";
+ s4.style.height="200px";
+ s4.style.backgroundColor="#0000ff";
+ s4.addEventListener('click',function s14(){
+    s4.style.backgroundColor="#ff0000";
+
+ });
+ let pss=document.getElementById('pass1');
+ let pss2=document.getElementById('pass2');
+ let spss=document.getElementById('sp1');
+ let spss1=document.getElementById('sp2');
+ pss.addEventListener('change',function(){
+    if (pss.value.length<=6){
+        spss.innerHTML="Too short";
+        spss.style.color="#f00"
+
+    }
+
+ })
+ pss2.addEventListener('change',function(){
+    if (pss.value!=pss2.value){
+        spss1.innerHTML="The two password dont match";
+        spss1.style.color="#f00"
+
+    }else{
+        document.getElementById('sub').style.display='block'
+
+    }
+
+ })
+ let btn=document.getElementById('song-btn');
+ let inp=document.getElementById('input-song');
+ btn.addEventListener('click', function(){
+    const newp=document.createElement('p');
+    newp.innerHTML=inp.value;
+    newp.classList.add("p-cls");
+    document.getElementById('song_cont').appendChild(newp);
+ });
 
  
